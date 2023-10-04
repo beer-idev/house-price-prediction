7daab048a135e96149a16e5b1665afb68a4ebabb
Beer idev <34055979+beer-idev@users.noreply.github.com>
2023-10-05 03:02:45 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
+import altair as alt
 
 PATH = 'web5.csv'
 
@@ -10,22 +12,25 @@ df = df.drop(["Unnamed: 0"], axis=1)
 st.sidebar.header("Filter")
 selected_location = st.sidebar.multiselect("Location", sorted(list(df.location.unique())), sorted(list(df.location.unique())))
 selected_bedroom = st.sidebar.selectbox("Bedrooms", sorted(list(df.bedroom.unique())))
-price_min = int(df['price'].min())
-price_max = int(df['price'].max())
-price_range = st.sidebar.slider("Price Range", price_min, price_max, (price_min, price_max), step=1000)
+# price_min = int(df['price'].min())
+# price_max = int(df['price'].max())
+# price_range = st.sidebar.slider("Price Range", price_min, price_max, (price_min, price_max), step=1000)
+price_input = st.sidebar.number_input('Price', min_value=0)
+
 
 
 # @st.cache
 def filter_data(locations, bedrooms, price, data): 
     data = data[(data.location.isin(locations))]
     data = data[data['bedroom'].isin([bedrooms])]
-    data = data[(data['price'] >= price[0]) & (data['price'] <= price[1])]
 
+    data['price_difference'] = abs(data['price'] - price)
+    data = data.sort_values(by='price_difference').head(10)
     return data
 
-df = filter_data(selected_location, selected_bedroom, price_range, df)
+df = filter_data(selected_location, selected_bedroom, price_input, df)
 st.header("Prediction Data")
-st.write(df.astype('object'))
+st.write(df)
 
 st.header("Price Trends")
 location_price_trends = df.groupby('location')['price'].sum()
